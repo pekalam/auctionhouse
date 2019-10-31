8361a34ec07b5a1a6ad9b2849ec69843c6126dbf
Marek Pękala <marekbf3@gmail.com>
2019-11-01 00:18:06 +0100

    filter categories refactor

--- front/src/app/auctions/components/auction-filters/auction-filters.component.ts
+++ front/src/app/auctions/components/auction-filters/auction-filters.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CategoryTreeNode } from '../../../core/models/CategoryTreeNode';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ConditionQuery, AuctionFilters } from '../../../core/queries/AuctionsQuery';
-import {BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+
+
+export interface FilterCategory {
+  link: any[];
+  value: string;
+  children: FilterCategory[];
+}
 
 
 @Component({
@@ -16,36 +23,36 @@ export class AuctionFiltersComponent implements OnInit {
 
   mobile = false;
 
-  @Input()
-  subcategories: CategoryTreeNode;
-
-  @Input()
-  selectedSubCategory: string;
-
   @Output()
   applyFilters = new EventEmitter<AuctionFilters>();
 
+  @Input()
+  filterCategories: FilterCategory;
+
+  @Input()
+  selectedCategoryValue: string;
+
   form: FormGroup;
-  selectedAuctionType: string = "3";
+  selectedAuctionType = '3';
 
 
   constructor(private formBuilder: FormBuilder, public breakpointObserver: BreakpointObserver) {
     this.breakpointObserver
-    .observe(['(max-width: 960px)'])
-    .subscribe((state: BreakpointState) => {
-      if (state.matches) {
-        this.mobile = true;
-      } else {
-        this.mobile = false;
-      }
-    });
+      .observe(['(max-width: 960px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.mobile = true;
+        } else {
+          this.mobile = false;
+        }
+      });
     this.form = formBuilder.group({
-      condition: "2",
+      condition: '2',
       type: this.selectedAuctionType,
-      minBuyNow: "0",
-      maxBuyNow: "0",
-      minAuction: "0",
-      maxAuction: "0"
+      minBuyNow: '0',
+      maxBuyNow: '0',
+      minAuction: '0',
+      maxAuction: '0'
     });
   }
 
@@ -57,9 +64,9 @@ export class AuctionFiltersComponent implements OnInit {
 
   applyClick() {
     if (this.form.valid) {
-      let filters = new AuctionFilters(this.form.value.condition, this.form.value.type, this.form.value.minBuyNow, this.form.value.maxBuyNow, this.form.value.minAuction, this.form.value.maxAuction);
-      if(this.lastFilters && this.lastFilters.equals(filters)){
-        //return;
+      const filters = new AuctionFilters(this.form.value.condition, this.form.value.type, this.form.value.minBuyNow, this.form.value.maxBuyNow, this.form.value.minAuction, this.form.value.maxAuction);
+      if (this.lastFilters && this.lastFilters.equals(filters)) {
+        // return;
       }
       this.lastFilters = filters;
       console.log(filters);
--- front/src/app/auctions/pages/auctions-page/auctions-page.component.ts
+++ front/src/app/auctions/pages/auctions-page/auctions-page.component.ts
@@ -6,6 +6,7 @@ import { AuctionsQuery, ConditionQuery, AuctionFilters } from '../../../core/que
 import { CategoriesQuery } from '../../../core/queries/CategoriesQuery';
 import { CategoryTreeNode } from '../../../core/models/CategoryTreeNode';
 import { Condition } from 'src/app/core/models/Product';
+import { FilterCategory } from '../../components/auction-filters/auction-filters.component';
 
 @Component({
   selector: 'app-auctions-page',
@@ -14,32 +15,57 @@ import { Condition } from 'src/app/core/models/Product';
 })
 export class AuctionsPageComponent implements OnInit {
   auctions: AuctionListModel[];
-  subCategoryTree: CategoryTreeNode;
+  filterCategories: FilterCategory;
+  selectedFilterCategoryValue: string;
   private currentCategory: Category;
 
 
   constructor(private activatedRoute: ActivatedRoute, private auctionsQuery: AuctionsQuery,
-    private categoriesQuery: CategoriesQuery) {
+              private categoriesQuery: CategoriesQuery) {
   }
 
-  private constructCategory(mainCategoryName: string, subCategoryName: string, subCategory2Name?: string) {
-    const cat: Category = { name: mainCategoryName, subCategory: null };
-    cat.subCategory = { name: subCategoryName, subCategory: null };
-    if (subCategory2Name) {
-      cat.subCategory.subCategory = { name: subCategory2Name, subCategory: null };
-    }
-    this.currentCategory = cat;
-    this.categoriesQuery.getSubcategoryTree(mainCategoryName, subCategoryName).subscribe((v) => {
-      this.subCategoryTree = {
-        categoryName: mainCategoryName,
-        subCategories: [v]
+  private createFilterCategories(mainCategoryName: string, treeNode: CategoryTreeNode) {
+    const subCategories2: FilterCategory[] = treeNode.subCategories.map<FilterCategory>((t) => {
+      const filterCat: FilterCategory = {
+        link: ['/auctions', mainCategoryName, treeNode.categoryName, t.categoryName],
+        value: t.categoryName,
+        children: []
       };
+      return filterCat;
     });
+    console.log(treeNode);
+
+    this.filterCategories = {
+      link: ['/category', mainCategoryName],
+      value: mainCategoryName,
+      children: subCategories2
+    };
+    console.log(this.filterCategories
+    );
+  }
+
+  private constructCategory(mainCategoryName: string, subCategoryName: string, subCategory2Name: string) {
+    const cat: Category = {
+      name: mainCategoryName,
+      subCategory: {
+        name: subCategoryName,
+        subCategory: {
+          name: subCategory2Name,
+          subCategory: null
+        }
+      }
+    };
+    this.currentCategory = cat;
+    this.categoriesQuery
+      .getSubcategoryTree(mainCategoryName, subCategoryName)
+      .subscribe((v) => this.createFilterCategories(mainCategoryName, v));
+
+    this.selectedFilterCategoryValue = subCategory2Name;
     this.fetchAuctions(0);
   }
 
-  applyFilters(filters: AuctionFilters){
-    console.log("applied");
+  applyFilters(filters: AuctionFilters) {
+    console.log('applied');
     console.log(filters);
 
     this.fetchAuctions(0, filters);
