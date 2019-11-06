d13d503c999cd7b4d85e8f8c486b42c41f984979
Marek Pękala <marekbf3@gmail.com>
2019-11-06 16:20:43 +0100

    test

--- backend/cli/build.sh
+++ backend/cli/build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ "$1" = "no-test" ] ; then
+	TEST="no-test"
+else
+	TEST="test"
+fi
+
+if [ "$TEST" = "test" ] ; then
+	echo "Running domain tests..."
+	dotnet test ../src/Core.DomainModelTests || { echo "Domain tests failed"; exit 1; }
+fi
+
+echo "Building update-server docker image..."
 pushd ../mongodb/views/docker
 docker build -f Dockerfile -t update-server .
 popd
