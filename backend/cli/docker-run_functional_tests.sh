f1d5c9cb4c7820622c2f63d9ff8e404da63e2ede
Marek Pękala <marekbf3@gmail.com>
2019-12-20 22:29:07 +0100

    more test logs

--- backend/cli/docker-run_functional_tests.sh
+++ backend/cli/docker-run_functional_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SUCCESS=1
-docker-compose -f ../docker-compose/docker-compose-functional-tests.yml up --build --exit-code-from auctionhouse-functional-tests auctionhouse-functional-tests || { SUCCESS=0; }
+docker-compose -f ../docker-compose/docker-compose-functional-tests.yml up --build --exit-code-from auctionhouse-functional-tests || { SUCCESS=0; }
 docker-compose -f ../docker-compose/docker-compose-functional-tests.yml down || { true; }
 
 if [ "$SUCCESS" = 0 ]; then
