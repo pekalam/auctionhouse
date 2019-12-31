a346532d943d1b208d1c422f051eccfeb4bd1edf
Marek Pękala <marekbf3@gmail.com>
2019-12-31 22:23:55 +0100

    sql server - db tests

---
+++ backend/src/Test.AuctionhouseDatabase/test insert_event when expected version equals 0 and event already exist and newVersion is null increments version.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[test insert_event when expected version equals 0 and event already exist and newVersion is null increments version]
+AS
+BEGIN
+    declare @count int;
+    declare @expectedCount int = 2;
+    declare @version bigint;
+    declare @expectedVersion bigint = 2;
+
+    EXEC tSQLt.FakeTable 'dbo.Events';
+
+    declare @aggId char(36) = N'10A2C48F-BABC-412C-B9BD-6CAA2B4C36BA';
+    insert into Events(Id, AggId, AggName, EventName, Date, Data, Version) values (NEWID(), @aggId, 'agg1', 'event 1', GETDATE(), '{"a": 1}', 1);
+
+    declare @now datetime2 = GETDATE();
+    execute dbo.insert_event @AggId = @aggId, @AggName = 'agg1', @EventName = 'event 1', @Date = @now, @Data = '{"a": 1}', @ExpectedVersion = 0, @NewVersion = NULL
+
+    select TOP 1 @version = e.Version from Events e order by e.Version desc;
+    select @count = count(*) from Events e;
+
+    EXEC tSQLt.AssertEquals @expectedVersion, @version;
+    EXEC tSQLt.AssertEquals @expectedCount, @count;
+END;
---
+++ backend/src/Test.AuctionhouseDatabase/test insert_event when expected version lower than 0 and events with given aggId doesnt exists inserts events.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[test insert_event when expected version lower than 0 and events with given aggId doesnt exists inserts events]
+AS
+BEGIN
+    declare @count int;
+    declare @expectedCount int = 1;
+    declare @insertedAggId varchar(100);
+    declare @version bigint;
+    declare @expectedVersion bigint = 1;
+
+    EXEC tSQLt.FakeTable @TableName = 'dbo.Events';
+
+    declare @aggId char(36) = N'10A2C48F-BABC-412C-B9BD-6CAA2B4C36BA';
+    declare @now datetime2 = GETDATE();
+    execute AuctionhouseDatabase.dbo.insert_event @AggId = @aggId, @AggName = 'agg1', @EventName = 'event 1', @Date = @now, @Data = '{"a": 1}', @ExpectedVersion = -1, @NewVersion = NULL
+
+    select @count = count(*) from Events e;
+    select TOP 1 @version = e.Version, @insertedAggId = e.AggId from Events e;
+
+    EXEC tSQLt.AssertEquals @expectedVersion, @version;
+    EXEC tSQLt.AssertEquals @aggId, @insertedAggId;
+    EXEC tSQLt.AssertEquals @expectedCount, @count;
+END;
