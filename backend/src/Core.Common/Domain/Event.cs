8a1413ce53f42b633e496b078bcb8e7afbe66bc8
Marek <marekbf3@gmail.com>
2021-12-20 13:58:43 +0100

    refactor(backend): added docs to UpdateEventGroup

--- backend/src/Core.Common/Domain/Event.cs
+++ backend/src/Core.Common/Domain/Event.cs
@@ -14,14 +14,21 @@ namespace Core.Common.Domain
         }
     }
 
+    /// <summary>
+    /// Event that can be part of <see cref="UpdateEventGroup"/>
+    /// </summary>
     public class UpdateEvent : Event
     {
         public UpdateEvent(string eventName) : base(eventName)
         {
-            AggVersion = -1;
+            AggVersion = -1; // agg versions is contained in UpdateEventGroup in which this UpdateEvent is contained
         }
     }
 
+    /// <summary>
+    /// Represents list of update events from aggregate. These are events which can be sent together as single <see cref="UpdateEventGroup"/>
+    /// instead of being sent separately.
+    /// </summary>
     public class UpdateEventGroup : Event
     {
         public Guid AggregateId { get; set; }
