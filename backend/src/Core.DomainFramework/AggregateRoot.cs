5f224f61784bbf89783e9813c516d4fbd55996a1
Marek <marekbf3@gmail.com>
2021-12-20 20:11:14 +0100

    refactor(backend): move domain base classes to framework project

---
+++ backend/src/Core.DomainFramework/AggregateRoot.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Common.Domain
+{
+    public abstract class AggregateRoot<T, U> : AggregateRoot<T> where T : AggregateRoot<T, U>, new() where U : UpdateEventGroup
+    {
+        private U _updateEventGroup = null;
+
+        protected void AddUpdateEvent(UpdateEvent @event)
+        {
+            if (_canAddNewEvents)
+            {
+                if (_updateEventGroup == null)
+                {
+                    _updateEventGroup = CreateUpdateEventGroup();
+                    _updateEventGroup.AggVersion = ++Version;
+                    _updateEventGroup.AggregateId = AggregateId;
+                    _pendingEvents.Add(_updateEventGroup);
+                }
+                _updateEventGroup.Add(@event);
+            }
+        }
+
+        protected abstract U CreateUpdateEventGroup();
+    }
+
+    public abstract class AggregateRoot<T> : IInternalEventAdd where T : AggregateRoot<T>, new()
+    {
+        public Guid AggregateId { get; protected set; } = Guid.NewGuid();
+
+        protected bool _canAddNewEvents = true;
+        protected List<Event> _pendingEvents = new List<Event>();
+        public IReadOnlyCollection<Event> PendingEvents => _pendingEvents;
+        public long Version { get; protected set; } = 0;
+
+        public static T FromEvents(IReadOnlyCollection<Event> events)
+        {
+            T t = new T();
+            t._canAddNewEvents = false;
+            foreach (var @event in events)
+            {
+                t.Apply(@event);
+            }
+
+            if (events.Count > 0)
+            {
+                t.Version = events.Last().AggVersion;
+            }
+
+            t._canAddNewEvents = true;
+            return t;
+        }
+
+        public void MarkPendingEventsAsHandled()
+        {
+            _pendingEvents.Clear();
+        }
+
+        protected void AddEvent(Event @event)
+        {
+            if (_canAddNewEvents)
+            {
+                @event.AggVersion = ++Version;
+                _pendingEvents.Add(@event);
+            }
+        }
+
+        protected abstract void Apply(Event @event);
+
+        void IInternalEventAdd.AddEvent(Event @event)
+        {
+            if (_canAddNewEvents)
+            {
+                _pendingEvents.Add(@event);
+            }
+        }
+    }
+}
---
+++ backend/src/Core.DomainFramework/Entity.cs
@@ -0,0 +1,55 @@
+﻿namespace Core.Common.Domain
+{
+    public interface IInternalEventAdd
+    {
+        void AddEvent(Event @event);
+    }
+
+    /// <summary>
+    /// Sublcass of <see cref="AggregateRoot{T}"/> can implement this interface to handle events sent from entities.
+    /// </summary>
+    public interface IInternalEventApply
+    {
+        void Apply(Event @event);
+    }
+
+    public abstract class Entity<TId>
+        where TId : ValueObject
+    {
+        private IInternalEventAdd _parentAdd;
+        private IInternalEventApply _parentApply;
+
+
+        /// <param name="parentAdd">In general usecase use <see cref="AddEvent(Event)"/>. Entity can use it to add event to parent ONLY if its not either applying event.</param>
+        /// <param name="parentApply">In general usecase use <see cref="AddEvent(Event)"/>. Can use it to apply event to parent. Usage of this param should be consistent in all constructors.</param>
+        protected Entity(IInternalEventAdd parentAdd, IInternalEventApply parentApply = null)
+        {
+            _parentAdd = parentAdd;
+            _parentApply = parentApply;
+        }
+
+        public TId Id { get; protected set; }
+
+        /// <summary>
+        /// Called by entity when changing its state in method.
+        /// </summary>
+        /// <param name="event"></param>
+        protected void AddEvent(Event @event)
+        {
+            _parentAdd.AddEvent(@event);
+            _parentApply?.Apply(@event);
+        }
+
+        /// <summary>
+        /// Called by parent when recreating entity state
+        /// </summary>
+        /// <param name="event"></param>
+        internal void ApplyInternal(Event @event)
+        {
+            Apply(@event);
+            _parentApply?.Apply(@event);
+        }
+
+        protected abstract void Apply(Event @event);
+    }
+}
