bb142b5b3780fd1150c6c838eb009cae22844156
Marek <marekbf3@gmail.com>
2021-12-20 13:37:46 +0100

    fix(test): updated docker images, scripts of functional tests
    * Updated docker images to .net core 3.1 runtime with alpine linux
    * fixed bugs in mongo scripts

--- backend/cli/docker-run_integration_tests.sh
+++ backend/cli/docker-run_integration_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SUCCESS=1
-docker-compose -f ../docker-compose/docker-compose-integration-tests.yml up --build --exit-code-from auctionhouse-integration-tests || { SUCCESS=0; }
+docker-compose -f ../docker-compose/docker-compose-integration-tests.yml up --abort-on-container-exit --build --exit-code-from auctionhouse-integration-tests || { SUCCESS=0; }
 docker-compose -f ../docker-compose/docker-compose-integration-tests.yml down || { true; }
 
 if [ "$SUCCESS" = 0 ]; then
--- backend/src/Test.IntegrationTests/mongo-standalone-entrypoint.sh
+++ backend/src/Test.IntegrationTests/mongo-standalone-entrypoint.sh
@@ -5,7 +5,7 @@ mongod --replSet rs0 --bind_ip_all &
 
 maxTry=10
 i=0
-while [ -z "`netcat -v -z 0.0.0.0 27017 2>&1 | grep open`" ] ; do
+while ! netcat -zv 0.0.0.0 27017; do
 	echo "Waiting for 27017 port to open";
 	sleep 3;
 	i=$((i+1))
@@ -19,7 +19,7 @@ mongo appDb /root/init.js
 
 maxTry=10
 i=0
-while [ -z "`netcat -v -z 0.0.0.0 27017 2>&1 | grep open`" ] ; do
+while ! netcat -zv 0.0.0.0 27017; do
 	echo "Waiting for 27017 port to open";
 	sleep 3;
 	i=$((i+1))
