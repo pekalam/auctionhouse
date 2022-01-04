ef2a398916a8ca5b592b9bb9d8245f90184b3510
Marek Pękala <marekbf3@gmail.com>
2022-01-04 01:54:03 +0100

    feat(backend): add XmlCategoryTreeStore adpater

---
+++ backend/src/Xml.CategoryTreeStore/XmlCategoryTreeStore.cs
@@ -0,0 +1,76 @@
+﻿using Core.Common.Domain.Categories;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlCategoryTreeStore
+{
+    public class XmlCategoryNameStoreSettings
+    {
+        public string CategoriesFilePath { get; set; }
+        public string SchemaFilePath { get; set; }
+    }
+
+    internal class XmlCategoryTreeStore : ICategoryTreeStore
+    {
+        private readonly XmlCategoryNameStoreSettings _categoryNameServiceSettings;
+        private readonly CategoryTreeNode _root = new CategoryTreeNode(null, 0, null);
+
+        public XmlCategoryTreeStore(XmlCategoryNameStoreSettings categoryNameServiceSettings)
+        {
+            _categoryNameServiceSettings = categoryNameServiceSettings;
+        }
+
+        private CategoryTreeNode ReadCategory(XmlReader xmlReader)
+        {
+            var categoryName = xmlReader.GetAttribute("name");
+            var categoryId = int.Parse(xmlReader.GetAttribute("id"));
+            var catNode = new CategoryTreeNode(categoryName, categoryId, _root);
+            xmlReader.ReadToDescendant("sub-category");
+            do
+            {
+                categoryName = xmlReader.GetAttribute("name");
+                categoryId = int.Parse(xmlReader.GetAttribute("id"));
+                var subCat = new CategoryTreeNode(categoryName, categoryId, catNode);
+                xmlReader.ReadToDescendant("sub-sub-category");
+                do
+                {
+                    categoryName = xmlReader.GetAttribute("name");
+                    categoryId = int.Parse(xmlReader.GetAttribute("id"));
+                    new CategoryTreeNode(categoryName, categoryId, subCat);
+                } while (xmlReader.ReadToNextSibling("sub-sub-category"));
+            } while (xmlReader.ReadToNextSibling("sub-category"));
+
+            return catNode;
+        }
+
+        public void Init()
+        {
+            var sc = new XmlSchemaSet();
+            sc.Add("categories-ns", _categoryNameServiceSettings.SchemaFilePath);
+
+            var settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add(sc);
+
+            using (var file = File.OpenRead(_categoryNameServiceSettings.CategoriesFilePath))
+            {
+                var xmlReader = XmlReader.Create(file, settings);
+
+                xmlReader.ReadToFollowing("categories");
+                if (xmlReader.Name != "categories")
+                {
+                    throw new Exception("Invalid root node name");
+                }
+
+                var i = 0;
+                while (xmlReader.ReadToFollowing("category"))
+                {
+                    ReadCategory(xmlReader);
+                }
+            }
+
+        }
+
+        public CategoryTreeNode GetCategoriesTree() => _root;
+    }
+}
---
+++ backend/src/Xml.CategoryTreeStore/XmlCategoryTreeStoreInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Core.Common.Domain.Categories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace XmlCategoryTreeStore
+{
+    public static class XmlCategoryTreeStoreInstaller
+    {
+        public static void AddXmlCategoryTreeStore(this IServiceCollection services, XmlCategoryNameStoreSettings settings)
+        {
+            services.AddSingleton(settings);
+            services.AddSingleton<ICategoryTreeStore, XmlCategoryTreeStore>();
+        }
+    }
+}
