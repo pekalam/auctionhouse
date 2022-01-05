511ce163ffc68301f2bbf23b10b8e2bd01306034
Marek Pękala <marekbf3@gmail.com>
2022-01-05 11:25:35 +0100

    refactor: Test.Auction.Base builders

--- backend/src/Test.Auctions.Base/Builders/GivenAuction.cs
+++ backend/src/Test.Auctions.Base/Builders/GivenAuction.cs
@@ -1,6 +1,5 @@
 ﻿using Auctions.Domain;
 using System;
-using static Test.Auctions.Domain.AuctionTestConstants;
 
 namespace Test.Auctions.Domain
 {
@@ -8,17 +7,7 @@ namespace Test.Auctions.Domain
     {
         public Auction ValidOfTypeBuyNowAndBid()
         {
-            var auctionArgsBuilder = new AuctionArgs.Builder()
-                .SetBuyNow(BUY_NOW_PRICE)
-                .SetStartDate(DateTime.UtcNow.AddMinutes(20))
-                .SetEndDate(DateTime.UtcNow.AddDays(5))
-                .SetOwner(UserId.New())
-                .SetProduct(new Product(PRODUCT_NAME, PRODUCT_DESCRIPTION, Condition.New))
-                .SetBuyNowOnly(false)
-                .SetTags(new[] { TAG_1 })
-                .SetName(NAME);
-            auctionArgsBuilder.SetCategories(CATEGORY_IDS, new TestConvertCategoryNamesToRootToLeafIds()).GetAwaiter().GetResult();
-            return new Auction(auctionArgsBuilder.Build());
+            return new Auction(new GivenAuctionArgs().ValidBuyNowAndBid());
         }
     }
 }
---
+++ backend/src/Test.Auctions.Base/Builders/GivenAuctionImage.cs
@@ -0,0 +1,13 @@
+﻿using Auctions.Domain;
+
+namespace Test.Auctions.Domain
+{
+    public class GivenAuctionImage
+    {
+        public AuctionImage Valid()
+        {
+            var imageId = Guid.NewGuid().ToString();
+            return new(imageId + "_sz1", imageId + "_sz2", imageId + "_sz3"); ;
+        }
+    }
+}
