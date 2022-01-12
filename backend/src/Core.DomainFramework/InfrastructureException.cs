657012bf59a0300a4896334cc5b33ffd5e6939dd
Marek Pękala <marekbf3@gmail.com>
2022-01-12 21:12:44 +0100

    fix: add missing InfrastructureException class

---
+++ backend/src/Core.DomainFramework/InfrastructureException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core.DomainFramework
+{
+
+    [Serializable]
+    public class InfrastructureException : Exception
+    {
+        public InfrastructureException() { }
+        public InfrastructureException(string message) : base(message) { }
+        public InfrastructureException(string message, Exception inner) : base(message, inner) { }
+        protected InfrastructureException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
