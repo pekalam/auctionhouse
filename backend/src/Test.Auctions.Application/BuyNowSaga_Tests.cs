54bdd5d9361f9727cc10b08a43461bdf0db974be
Marek Pękala <marekbf3@gmail.com>
2022-01-20 11:21:47 +0100

    test: remove unused code in Test.Auctions.Application

--- backend/src/Test.Auctions.Application/BuyNowSaga_Tests.cs
+++ backend/src/Test.Auctions.Application/BuyNowSaga_Tests.cs
@@ -22,55 +22,12 @@ using System.Threading.Tasks;
 using Test.Auctions.Base.Builders;
 using Test.Auctions.Base.Mocks;
 using Test.Common.Base.Mocks;
+using Test.Common.Base.Mocks.Events;
 using Xunit;
 using static Auctions.DomainEvents.Events.V1;
 
 namespace Test.Auctions.Application
 {
-    public class TestAppEvent<T> : IAppEvent<T> where T : Event
-    {
-        public CommandContext CommandContext { get; set; }
-
-        public T Event { get; set; }
-
-        public ReadModelNotificationsMode ReadModelNotifications { get; set; }
-    }
-
-    internal class TestAppEventBuilder : IAppEventBuilder
-    {
-        private CommandContext _commandContext;
-        private ReadModelNotificationsMode _readModelNotificationsMode;
-        private Event _event;
-
-        public IAppEvent<TEvent> Build<TEvent>() where TEvent : Event
-        {
-            return new TestAppEvent<TEvent>
-            {
-                Event = (TEvent)_event,
-                CommandContext = _commandContext,
-                ReadModelNotifications = _readModelNotificationsMode,
-            };
-        }
-
-        public IAppEventBuilder WithCommandContext(CommandContext commandContext)
-        {
-            _commandContext = commandContext;
-            return this;
-        }
-
-        public IAppEventBuilder WithEvent<TEvent>(TEvent @event) where TEvent : Event
-        {
-            _event = @event;
-            return this;
-        }
-
-        public IAppEventBuilder WithReadModelNotificationsMode(ReadModelNotificationsMode consistencyMode)
-        {
-            _readModelNotificationsMode = consistencyMode;
-            return this;
-        }
-    }
-
     public class BuyNowSaga_Tests
     {
         InMemoryAuctionRepository _auctions = new();
