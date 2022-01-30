f44b1a2283b96a69f6e95c6a03ccd5e38579d404
Marek Pękala <marekbf3@gmail.com>
2022-01-30 23:38:50 +0100

    chore: removed unused cli scripts

--- backend/cli/local-run_domain_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Running domain tests..."
-dotnet test ../src/Test.DomainModelTests || { echo "Domain tests failed"; exit 1; }
--- backend/cli/local-run_unit_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Running unit tests..."
-dotnet test ../src/Test.UnitTests -v n || { echo "Domain tests failed"; exit 1; }
