bac54707eef47cd0ec89d955391edd3c23826a7a
Marek Pękala <marekbf3@gmail.com>
2022-02-04 13:13:07 +0100

    fix: docker stack issues
    * update invalid healthcheck script for AuctionhouseDatabase proj
    * update dns names of web API containers
    * fix docker-compose files

--- backend/src/AuctionhouseDatabase.Docker/healthcheck.sh
+++ backend/src/AuctionhouseDatabase.Docker/healthcheck.sh
@@ -6,4 +6,6 @@ if [ -e "/run/secrets/sql_server_password" ]; then
 	SA_PASSWORD=`< /run/secrets/sql_server_password`
 fi
 
-/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P "$SA_PASSWORD" -d AuctionhouseDatabase -Q "select distinct 1 from dbo.Events"
+if nc -zv 127.0.0.1 32112; then
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P "$SA_PASSWORD" -d AuctionhouseDatabase -Q "select distinct 1 from dbo.Event"
+fi
