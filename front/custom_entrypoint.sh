4027b5c647b9d0b9c4fe9980f867c1920ad9e6b2
Marek <marekbf3@gmail.com>
2022-04-09 21:34:35 +0200

    feat: update front cert source

--- front/custom_entrypoint.sh
+++ front/custom_entrypoint.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 set -e
 
+start_nginx(){
+    echo "Executing nginx entrypoint"
+    exec /docker-entrypoint.sh "$@"
+}
+
+if [ -e "/run/secrets/front_ssl_chain" ]; then
+    echo "Found front_ssl_chain secret"
+    cp /run/secrets/front_ssl_chain /usr/share/nginx/ssl_certs/chain.txt
+
+    if [ -e "/run/secrets/front_ssl_private" ]; then
+        echo "Found front_ssl_private secret"
+        cp /run/secrets/front_ssl_private /usr/share/nginx/ssl_certs/private.txt
+    else
+        echo "Couldn't find front_ssl_private"
+        exit 64
+    fi
+
+    start_nginx $@
+fi
+
+
 if [ -n "$SSL_CHAIN" ]; then
     echo "Found SSL_CHAIN variable"
+    echo "$SSL_CHAIN" > /usr/share/nginx/ssl_certs/chain.txt
 else
     echo "SSL_CHAIN variable not found. Executing nginx entrypoint"
-    exec /docker-entrypoint.sh "$@"
+    start_nginx $@
 fi
 if [ -n "$SSL_PRIVATE" ]; then
     echo "Found SSL_PRIVATE variable"
+    echo "$SSL_PRIVATE" > /usr/share/nginx/ssl_certs/private.txt
 else
     exit 128;
 fi
 
-echo "writing ssl certs"
-echo "$SSL_CHAIN" > /usr/share/nginx/ssl_certs/chain.txt
-echo "$SSL_PRIVATE" > /usr/share/nginx/ssl_certs/private.txt
-
-echo "Executing nginx entrypoint"
-exec /docker-entrypoint.sh "$@"
+start_nginx $@
