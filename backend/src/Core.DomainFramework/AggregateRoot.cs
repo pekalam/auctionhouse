964705ba193d2f94a8198274ae9d90843821cde2
Marek Pękala <marekbf3@gmail.com>
2022-06-30 23:08:25 +0200

    refactor: split aggregate root class

--- backend/src/Core.DomainFramework/AggregateRoot.cs
+++ backend/src/Core.DomainFramework/AggregateRoot.cs
@@ -4,42 +4,6 @@ using System.Linq;
 
 namespace Core.Common.Domain
 {
-    namespace Default
-    {
-        public abstract class AggregateRoot<T, U> : AggregateRoot<T, Guid, U>
-            where T : AggregateRoot<T, Guid, U>, new() where U : UpdateEventGroup
-        {
-
-        }
-
-
-        public abstract class AggregateRoot<T> : Domain.AggregateRoot<T, Guid> where T : Domain.AggregateRoot<T, Guid>, new()
-        {
-        }
-    }
-
-    public abstract class AggregateRoot<T, TId, U> : AggregateRoot<T, TId> where T : AggregateRoot<T, TId, U>, new() where U : UpdateEventGroup
-    {
-        private U _updateEventGroup = null;
-
-        protected void AddUpdateEvent(UpdateEvent @event)
-        {
-            if (_canAddNewEvents)
-            {
-                if (_updateEventGroup == null)
-                {
-                    _updateEventGroup = CreateUpdateEventGroup();
-                    _updateEventGroup.AggVersion = ++Version;
-                    _updateEventGroup.AggregateId = Guid.Parse(AggregateId.ToString()); //TODO
-                    _pendingEvents.Add(_updateEventGroup);
-                }
-                _updateEventGroup.Add(@event);
-            }
-        }
-
-        protected abstract U CreateUpdateEventGroup();
-    }
-
     public abstract class AggregateRoot<T, TId> : IInternalEventAdd where T : AggregateRoot<T, TId>, new()
     {
         static AggregateRoot()
