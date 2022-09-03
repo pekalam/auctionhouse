f23e9f774ba7140f7446a579bb3c033537c32788
Marek Pękala <marekbf3@gmail.com>
2022-09-03 16:40:40 +0200

    refactor: move notifications enum to separate file

---
+++ backend/src/Modules/Common/Common.Application/Events/ReadModelNotificationsMode.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.Application.Events
+{
+    public enum ReadModelNotificationsMode
+    {
+        /// <summary>
+        /// Query listener can handle event of this type and send notification about completion / failure immediately.
+        /// </summary>
+        Immediate,
+        /// <summary>
+        /// Query listener must mark unhandled event of corresponding <see cref="RequestStatus"/> as handled. If <see cref="RequestStatus"/> 
+        /// is marked as completed and all events are handled then handler can send notification about completion / failure.
+        /// </summary>
+        Saga,
+        /// <summary>
+        /// Query listener should not send any notification.
+        /// </summary>
+        Disabled,
+    }
+}
