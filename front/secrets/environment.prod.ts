d149268b1d2fe620eeb2cf1429db23af366a66d2
Marek Pękala <marekbf3@gmail.com>
2023-01-06 13:52:06 +0100

    feat: move deployment part to template

--- front/secrets/environment.prod.ts
+++ front/secrets/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  API_URL: window['API_URL'] || 'https://pekalam.me',
-  WS_URL: window['WS_URL'] || 'https://pekalam.me',
+  API_URL: window['API_URL'] || 'https://pekalam.online',
+  WS_URL: window['WS_URL'] || 'https://pekalam.online',
   serverDemoModeEnabled: (window.hasOwnProperty('serverDemoModeEnabled') ? window['serverDemoModeEnabled'] : true),
 };
